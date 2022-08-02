cc1173b9710572939bbd14a8af55445819f0972c
Júlia Moniz de Aragão Alves <juliamonizaa@gmail.com>
2022-08-02 15:42:54 -0300

    :poop: juju plantão ajeitando o rejax

--- labefood2/src/pages/SignUpPage/SignUpForm.js
+++ labefood2/src/pages/SignUpPage/SignUpForm.js
@@ -19,11 +19,11 @@ export default function SignUpForm() {
    <FormContainer onSubmit={onSubmitForm}>
       <InputStyled placeholder="Nome e sobrenome" name={"name"} value={form.name} onChange={onChange} type={"text"} required/>
       <Separator height={"16px"} />
-      <InputStyled placeholder="email@email.com" name={"email"} value={form.email} onChange={onChange} type={"email"} required/>
+      <InputStyled placeholder="email@email.com" name={"email"} value={form.email} onChange={onChange} type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
       <Separator height={"16px"} />
-      <InputStyled placeholder="000.000.000-00" name={"cpf"} value={form.cpf} onChange={onChange} type={""} required/>
+      <InputStyled placeholder="000.000.000-00" name={"cpf"} value={form.cpf} onChange={onChange} pattern="[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}" required/>
       <Separator height={"16px"} />
-      <InputStyled placeholder="Mínimo 6 caracteres" name={"password"} value={form.password} onChange={onChange} type={"password"} required/>
+      <InputStyled placeholder="Mínimo 6 caracteres" name={"password"} value={form.password} onChange={onChange} type="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required/>
       <Separator height={"16px"} />
       <InputStyled placeholder="tenho que ver como fazer isso"/>
       <Separator height={"16px"} />
