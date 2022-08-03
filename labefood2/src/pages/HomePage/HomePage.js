34f350c25009395bd73fb5a01c7de10831d6e513
Murilo Caun Henrique <mcchenrique2101@gmail.com>
2022-08-03 18:06:36 -0300

    :construction: Category Filter

--- labefood2/src/pages/HomePage/HomePage.js
+++ labefood2/src/pages/HomePage/HomePage.js
@@ -3,18 +3,31 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { CardHome } from "../../components/CardHome/CardHome";
 import { Separator } from "../../styled";
-import { NewInput, FilterContainer, Filter } from "./styled";
+import {
+  NewInput,
+  FilterContainer,
+  Filter,
+  CategoryListAll,
+  CategoryListFilter,
+} from "./styled";
 import { useProtectedPage } from "../../hooks/useProtectedPage";
 import { GlobalContext } from "../../global/GlobalContext";
 
 export default function HomePage() {
   //useProtectedPage()
 
-  const {restaurantsArray} = useContext(GlobalContext);
-  const restauranteList = restaurantsArray?.map((item) => {
+  const { restaurantsArray } = useContext(GlobalContext);
+
+  const restaurantsListCategory = restaurantsArray?.map((item) => {
     return (
-      <CardHome key={item.id} item={item}/>
-    )
+      <Filter key={item.id} item={item.category} style={{ color: "black" }}>
+        {item.category}
+      </Filter>
+    );
+  });
+
+  const restaurantsList = restaurantsArray?.map((item) => {
+    return <CardHome key={item.id} item={item} />;
   });
 
   return (
@@ -22,15 +35,12 @@ export default function HomePage() {
       <Header title={"FutureEats"} type={"semSeta"} width={"143.5px"} />
       <Separator height={"8px"} />
       <NewInput placeholder="Restaurante" />
-      <Separator height={"20px"} />
+      <Separator height={"8px"} />
       <FilterContainer>
-        <Filter>Burguer</Filter>
-        <Filter>Asiática</Filter>
-        <Filter>Massas</Filter>
-        <Filter>Saudável</Filter>
-        <Filter>Churrasco</Filter>
+        <CategoryListAll>Todos</CategoryListAll>
+        <CategoryListFilter>{restaurantsListCategory}</CategoryListFilter>
       </FilterContainer>
-      {restauranteList}
+      {restaurantsList}
       <Footer />
     </div>
   );
