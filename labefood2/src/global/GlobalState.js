77d1dd34ec2af764cb312f2139ddba2254cd511e
Myllena Moreira Miranda <myllenamiranda1411@gmail.com>
2022-08-02 23:49:59 -0300

    Criação de componente AddCart

--- labefood2/src/global/GlobalState.js
+++ labefood2/src/global/GlobalState.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { goToLogin } from "../routes/coordinator";
 import { GlobalContext } from "./GlobalContext";
 
 export const GlobalState = (props) => {
   const Provider = GlobalContext.Provider;
+  const [add, setAdd] = useState(false);
   const logout = (navigate) => {
     localStorage.setItem("token", "");
     goToLogin(navigate);
   };
-  const values = {logout}
+  const values = { logout };
   return <Provider value={values}>{props.children}</Provider>;
 };
