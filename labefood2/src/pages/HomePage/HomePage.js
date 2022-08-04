07ea44c9c71feb5f52a4b8a914de1b686cd00af3
Murilo Caun Henrique <mcchenrique2101@gmail.com>
2022-08-04 15:04:59 -0300

    :rocket: Filtro de categoria

--- labefood2/src/pages/HomePage/HomePage.js
+++ labefood2/src/pages/HomePage/HomePage.js
@@ -1,44 +1,76 @@
-import React, { useState,useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { CardHome } from "../../components/CardHome/CardHome";
 import { Separator } from "../../styled";
-
-import { NewInput, FilterContainer, Filter, HomeContainer, CategoryListAll, CategoryListFilter, } from "./styled";
-
+import {
+  NewInput,
+  FilterContainer,
+  Filter,
+  HomeContainer,
+  CategoryListAll,
+  CategoryListFilter,
+} from "./styled";
 import { useProtectedPage } from "../../hooks/useProtectedPage";
 import { GlobalContext } from "../../global/GlobalContext";
 
+
 export default function HomePage() {
   //useProtectedPage()
-  const [query,setQuery]=useState("")
+  const [query, setQuery] = useState("");
   const { restaurantsArray } = useContext(GlobalContext);
+  const [selectCategory, setSelectCategory] = useState("");
 
   const restaurantsListCategory = restaurantsArray?.map((item) => {
     return (
-      <Filter key={item.id} item={item.category} style={{ color: "black" }}>
+      <Filter
+        key={item.id}
+        item={item.category}
+        onClick={() => {
+          onClickCategory(item.category);
+        }}
+      >
         {item.category}
       </Filter>
     );
   });
+  const onClickCategory = (e) => {
+    setSelectCategory(e);
+  };
+  console.log(selectCategory);
+  const restaurantsList = restaurantsArray
+    ?.filter((item) => {
+      return item.name.toLowerCase().includes(query);
+    })
+    .filter((item) => {
+      return item.category.includes(selectCategory);
+    })
+    .map((item) => {
+      return <CardHome key={item.id} item={item} />;
+    });
+  const onChangeQuery = (event) => {
+    setQuery(event.target.value);
+  };
 
-  const restaurantsList = restaurantsArray?.filter((item)=>{
-    return item.name.toLowerCase().includes(query)
-  }).map((item) => {
-    return <CardHome key={item.id} item={item} />;
-  });
-  const onChangeQuery= (event)=>{
-    setQuery(event.target.value)
-  }
-  console.log(query)
+  console.log(query);
   return (
     <HomeContainer>
       <Header title={"FutureEats"} type={"semSeta"} width={"143.5px"} />
       <Separator height={"8px"} />
-      <NewInput placeholder="Restaurante" value={query} onChange={onChangeQuery}/>
+      <NewInput
+        placeholder="Restaurante"
+        value={query}
+        onChange={onChangeQuery}
+      />
       <Separator height={"8px"} />
       <FilterContainer>
-        <CategoryListAll>Todos</CategoryListAll>
+        <CategoryListAll
+          onClick={() => {
+            onClickCategory("");
+          }}
+        >
+          Todos
+        </CategoryListAll>
         <CategoryListFilter>{restaurantsListCategory}</CategoryListFilter>
       </FilterContainer>
       {restaurantsList}
